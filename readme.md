1d7c5bf05ffa883b55f70539f860f5547a125e8c
sivelswhy <82051273+sivelswhy@users.noreply.github.com>
2022-04-02 20:18:26 +0200

    Version 1.2.0
    See changes on the NPM page

--- readme.md
+++ readme.md
@@ -7,8 +7,16 @@
 YouTube Downloading is a fast and easy way to download videos from YouTube. All Downloaded videos will be put in your local download directory.
 
 ### Usage
+
 To use this module, just do `npx youtube-downloading` and follow the instructions.
 
-Don't hesitate to improve this package on [gituhb](https://github.com/sivelswhy/youtube-downloader)
+Don't hesitate to improve this package on [github](https://github.com/sivelswhy/youtube-downloader)
 
 #### Be kind, this is my first package :)
+
+#### Recent changes
+
+- <u>**1.2.0 :**</u> Made the code only available for Windows (cause i'm lazy to make it work on other platforms)
+- Added file name checker (if it contains carachters that are not allowed in windows file names)
+- Added Checking if the video is private or unlisted (for privacy reasons)
+- <u>**1.1.0 :**</u> Added a URL validation (if it's a valid youtube URL)
--- src/index.js
+++ src/index.js
@@ -6,17 +6,26 @@ import chalkAnimation from "chalk-animation";
 import { createSpinner } from "nanospinner";
 import ytdl from "ytdl-core";
 import fs from "fs";
-import {username} from 'username';
+import { username } from "username";
 
 let linkvalue;
 
 const sleep = (ms = 3000) => new Promise((r) => setTimeout(r, ms));
-let user = await username()
+let user = await username();
 async function welcome() {
+  if (process.platform !== "win32") {
+    console.log(
+      chalk.red(
+        'Currently only Windows is supported, thank u for your understanding ❤️'
+      )
+    );
+    process.exit(1)
+  }
   console.clear();
   const Title = chalkAnimation.neon(
     `Welcome to the Downloader of YouTube video with the Terminal\n`
   );
+
   await sleep();
   Title.stop();
   console.log(`
@@ -24,8 +33,7 @@ async function welcome() {
   console.log(`
                 ${chalk.cyan`https://github.com/sivelswhy`}\n`);
 }
-
-async function askLink() {
+async function askLink(link) {
   const answers = await inquirer.prompt({
     name: "linkfield",
     type: "input",
@@ -33,49 +41,88 @@ async function askLink() {
     default() {
       return console.log(
         "Please insert YouTube video link \n " +
-          chalk.italic(
-            "(right click in terminal to paste from clipboard)"
-          )
+          chalk.italic("(right click in terminal to paste from clipboard)")
       );
     },
   });
-  // console.log('Le lien est', answers.linkfield)
-  linkvalue = answers.linkfield
-  if (!linkvalue.includes(`youtu`)) {
-    console.log(chalk.red(`Your URL is not a valid YouTube link`));
+  linkvalue = answers.linkfield;
+  const validate = ytdl.validateURL(linkvalue);
+  if (validate === false) {
+    console.log(chalk.red.underline(`\nYour URL is not a valid one !\n`));
     process.exit(1);
   }
+    //check if a url is a public video
+    const infolink = await ytdl.getBasicInfo(linkvalue);
+    if (infolink.videoDetails.isUnlisted === true || infolink.videoDetails.isPrivate === true) {
+    //if (infolink.isPrivate === false || infolink.isUnlisted === false) {
+      console.log(chalk.red(`\n🚨 For privacy reasons, we do not allow to download private or unlisted videos🚨\n`));
+      process.exit(1);
+    }
 }
+
 async function fileType() {
   const answerFile = await inquirer.prompt({
     name: "file_type",
     type: "list",
-    message:
-      "In which file type do you want to store the video",
-    choices: [
-      ".mp3",
-      ".mp4",
-    ],
+    message: "In which file type do you want to store the video",
+    choices: [".mp3", ".mp4"],
   });
-  return answerFile.file_type
+  return answerFile.file_type;
 }
 async function downloadvid() {
-  const spinner = createSpinner(
-    "The video is being downloaded."
-    ).start();  
+  const spinner = createSpinner("The video is being downloaded.").start();
   const stream = ytdl(`${linkvalue}`);
-  const infolink = await ytdl.getBasicInfo(linkvalue)
-  const video_title = (infolink.videoDetails.title)
-  const newvideo_title = video_title.replace(/\s+/g, '-').toLowerCase();
-  const newvideo_titlex2 = newvideo_title.replace('?', '-').replace('\\', '-').replace('/', '-').replace(':', '-').replace('*', '-').replace('"', '-').replace('<', '-').replace('>', '-').replace('|', '-')
-  stream.pipe(fs.createWriteStream(`C:/Users/${user}/Downloads/${newvideo_titlex2}${value}`));
-  stream.on("finish", function() {
-      spinner.success({ text: "The video is now downloaded !\n" });
-      console.log(`You can now find the file under C:/Users/${user}/Downloads/${newvideo_titlex2}${value}`)
+  const infolink = await ytdl.getBasicInfo(linkvalue);
+  const video_title = infolink.videoDetails.title;
+  const newvideo_title = video_title.replace(/\s+/g, "-").toLowerCase();
+  let newvideo_titlex2 = newvideo_title
+    .replace("?", "-")
+    .replace("\\", "-")
+    .replace("/", "-")
+    .replace(":", "-")
+    .replace("*", "-")
+    .replace('"', "-")
+    .replace("<", "-")
+    .replace(">", "-")
+    .replace("|", "-")
+    .replace("?", "-")
+    .replace("\\", "-")
+    .replace("/", "-")
+    .replace(":", "-")
+    .replace("*", "-")
+    .replace('"', "-")
+    .replace("<", "-")
+    .replace(">", "-")
+    .replace("|", "-")
+    .replace("?", "-")
+    .replace("\\", "-")
+    .replace("/", "-")
+    .replace(":", "-")
+    .replace("*", "-")
+    .replace('"', "-")
+    .replace("<", "-")
+    .replace(">", "-")
+    .replace("|", "-");
+  //if the newvideo_titlex2 contains multiple dashes following each other, it will be replaced by one dash
+  newvideo_titlex2 = newvideo_titlex2.replace(/-{2,}/g, "-");
+  //if the newvideo_titlex2 finishes with a dash, it will be removed
+  if (newvideo_titlex2.endsWith("-")) {
+    newvideo_titlex2 = newvideo_titlex2.slice(0, -1);
+  }
+  stream.pipe(
+    fs.createWriteStream(
+      `C:/Users/${user}/Downloads/${newvideo_titlex2}${value}`
+    )
+  );
+  stream.on("finish", function () {
+    spinner.success({ text: "The video is now downloaded !\n" });
+    console.log(
+      `You can now find the file under C:/Users/${user}/Downloads/${newvideo_titlex2}${value}`
+    );
   });
 }
 await welcome();
-const linkask = await askLink()
-const value = await fileType() 
+const linkask = await askLink();
+const value = await fileType();
 console.log(`\n\n`);
 await downloadvid();
